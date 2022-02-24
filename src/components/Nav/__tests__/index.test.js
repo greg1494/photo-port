a25faa1efdbd183abf7bc900e8da8cffa026d7a6
Greg Christiansen <gregchristiansen14@gmail.com>
2022-02-24 16:23:15 -0600

    added the contact form

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,15 +5,33 @@ import Nav from "..";
 
 afterEach(cleanup);
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
 describe('Nav component', () => {
     // Baseline Test
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+         />);
     });
     // Snapshot Test
     it('matches snapshot', () => {
         // render Nav
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+         />);
         // assert value comparison
         expect (asFragment()).toMatchSnapshot();
     });
@@ -22,7 +40,11 @@ describe('Nav component', () => {
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         // Arrange
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         // Assert
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     });
@@ -31,7 +53,11 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
     it('inserts text into the links', () => {
       // Arrange
-      const { getByTestId } = render(<Nav />);
+      const { getByTestId } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+     />);
       // Assert
       expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
       expect(getByTestId('about')).toHaveTextContent('About me');
